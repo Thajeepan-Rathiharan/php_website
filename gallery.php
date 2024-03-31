a2d8a97742498cd60c66973f41145ace9c19660a
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-03-31 15:25:18 +0100

    Updated gallery.php with content

---
+++ gallery.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>blood donation|gallery</title>
+<link rel="stylesheet" href="styles.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+   <header>
+      <nav>
+	    <h1>BLOOD negative</h1>
+		<ul id="nav">
+		   <li><a class="st" href="index.php">HOME</a></li>
+		   <li><a class="st" href="about.php">ABOUT US</a></li>
+		   <li><a class="active" href="#">GALLERY</a></li>
+		   <li><a class="st" href="contact.php">CONTACT</a></li>
+		</ul>
+	  </nav>
+   </header>
+<div class="divider"></div>
+
+<div class="image">
+  <div class="row  justify-content-evenly">
+    <div class="col-6 ">
+	<img class="img-fluid" src="image/a1.jpg">
+    </div>
+    <div class="col-6 ">
+	<img class="img-fluid" src="image/b2.jpg">
+    </div>
+    <div class="col-6 ">
+	<img class="img-fluid" src="image/c3.jpg">
+    </div>
+    <div class="col-6 ">
+	<img class="img-fluid" src="image/d4.jpg">
+    </div>
+  </div>
+</div>
+
+
+ <div class="divider"></div>
+<footer>
+copyrights &copy reserved by BLOODnegative since 2019.<br>
+<u>support@bloodnegative.com</u>
+</footer>
+</body>
+</html>
