d1bcc2c04f9e9d8fd4d5a0d1cda8ef72ce05cfad
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-03-31 15:19:30 +0100

    Made JavaScript folder and added function to it

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function lettersOnly(input){
+	var regex=/[^a-z]/gi;
+	input.value=input.value.replace(regex,"");
+}
+function digitOnly(input){
+	var regex=/[^0-9]/;
+	input.value=input.value.replace(regex,"");
+}
+	
